ff71a0329ffc617860ff8d19cf3ac363190adaa3
lpcruz <lpcruz16@misena.edu.co>
2021-10-11 19:41:11 -0500

    cambio de nombres carpetas

--- contacto.php
+++ contacto.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../portafolio/css/contacto.css">
+  <link rel="stylesheet" href="../portafolio/estilos/contacto.css">
   <title>Contacto💡</title>
 </head>
 <body>
--- experiencia.php
+++ experiencia.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../portafolio/css/exp.css">
+  <link rel="stylesheet" href="../portafolio/estilos/exp.css">
   <title>Mi experiencia 💡 </title>
 </head>
 <body>
